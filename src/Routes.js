9c2a646151476c09d62de8c053e1086b49e3a888
Jake <jakebirkes@gmail.com>
2020-08-16 21:27:10 -0700

    adding Layout wrapper

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Layout } from './components/Layout/Layout';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Missing } from './pages/Missing';
 
 export const Routes = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route component={Missing}/>
-    </Switch>
-  </Router>
+  <Layout>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route component={Missing}/>
+      </Switch>
+    </Router>
+  </Layout>
 );
