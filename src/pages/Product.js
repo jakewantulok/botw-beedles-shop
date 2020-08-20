36318ea0d559e5514a7be419546325bb71284d8e
Jake <jakebirkes@gmail.com>
2020-08-19 20:55:44 -0700

    WIP Product page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Layout } from '../components/Layout/Layout';
+import { CartContext } from '../context/CartContext';
+import SetImg from '../components/SetImg/SetImg';
 
-export const Product = () => (
-  <div>Product page</div>
-);
+export const Product = () => {
+
+    const { viewItem } = useContext(CartContext);
+
+    const sizeBtns = () => viewItem.sizes.map(s => {
+        return (
+            s.inventory > 0 ? <button className="btn btn-outline-primary btn-sm" key={s.size}>{s.size}</button> : null
+        );
+    });
+
+    console.log(viewItem.sizes);
+    return ( 
+        <Layout title={viewItem.name} description={viewItem.name} >
+            <>
+                <div className="text-center mt-5">
+                    <h1>PDP</h1>
+                    <p>This is the PDP.</p>
+                    <p>{viewItem.name}</p>
+                    {/* <SetImg info={viewItem} addClass='img-fluid d-block'/> */}
+                    {sizeBtns()}
+                </div>
+                
+            </>
+        </Layout>
+     );
+}
