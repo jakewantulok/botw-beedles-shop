cf5852b15bd56425fab4aed3de3ae7ded885b3e5
Jake <jakebirkes@gmail.com>
2020-08-19 20:55:33 -0700

    init Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Layout } from '../components/Layout/Layout';
+import ProductsGrid from '../components/ProductGrid/ProductGrid';
 
-export const Home = () => (
-  <div>Home page</div>
+export const Home = () => ( 
+  <Layout title="Store" description="This is the Store page" >
+      <div>
+          <div className="text-center mt-5">
+              <h1>Store</h1>
+              <p>This is the Store Page.</p>
+          </div>
+          <ProductsGrid/>
+      </div>
+  </Layout>
 );
