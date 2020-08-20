04d012be6e53cb37544701719c72af53cd21729c
Jake <jakebirkes@gmail.com>
2020-08-19 17:12:04 -0700

    dummy data object for projects setup

---
+++ src/data/productData.js
@@ -0,0 +1,310 @@
+export const dummyProducts = [
+  {
+    id: 0,
+    name: "Product 2",
+    price: 39.99,
+    photo: "1.jpg",
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: 5
+      },
+      {
+        size: 'small',
+        inventory: 12
+      },
+      {
+        size: 'medium',
+        inventory: 20
+      },
+      {
+        size: 'large',
+        inventory: 25
+      },
+      {
+        size: 'x-large',
+        inventory: 20
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: "Product",
+    price: 57.99,
+    photo: "2.jpg",
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: 5
+      },
+      {
+        size: 'small',
+        inventory: 12
+      },
+      {
+        size: 'medium',
+        inventory: 20
+      },
+      {
+        size: 'large',
+        inventory: 25
+      },
+      {
+        size: 'x-large',
+        inventory: 20
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Product 3",
+    price: 49.99,
+    photo: "3.jpg",
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: 5
+      },
+      {
+        size: 'small',
+        inventory: 12
+      },
+      {
+        size: 'medium',
+        inventory: 20
+      },
+      {
+        size: 'large',
+        inventory: 25
+      },
+      {
+        size: 'x-large',
+        inventory: 20
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: "Product 4",
+    price: 89.99,
+    photo: "4.jpg",
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: 5
+      },
+      {
+        size: 'small',
+        inventory: 12
+      },
+      {
+        size: 'medium',
+        inventory: 20
+      },
+      {
+        size: 'large',
+        inventory: 25
+      },
+      {
+        size: 'x-large',
+        inventory: 20
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: "Product 5",
+    price: 75.95,
+    photo: "5.jpg",
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: 5
+      },
+      {
+        size: 'small',
+        inventory: 12
+      },
+      {
+        size: 'medium',
+        inventory: 20
+      },
+      {
+        size: 'large',
+        inventory: 25
+      },
+      {
+        size: 'x-large',
+        inventory: 20
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: "Product 6",
+    price: 39.49,
+    photo: "6.jpg",
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: 5
+      },
+      {
+        size: 'small',
+        inventory: 12
+      },
+      {
+        size: 'medium',
+        inventory: 20
+      },
+      {
+        size: 'large',
+        inventory: 25
+      },
+      {
+        size: 'x-large',
+        inventory: 20
+      }
+    ]
+  },
+  {
+    id: 6,
+    name: "Product 7",
+    price: 69.70,
+    photo: "7.jpg",
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: 5
+      },
+      {
+        size: 'small',
+        inventory: 12
+      },
+      {
+        size: 'medium',
+        inventory: 20
+      },
+      {
+        size: 'large',
+        inventory: 25
+      },
+      {
+        size: 'x-large',
+        inventory: 20
+      }
+    ]
+  },
+  {
+    id: 7,
+    name: "Product 8",
+    price: 34.50,
+    photo: "8.jpg",
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: 5
+      },
+      {
+        size: 'small',
+        inventory: 12
+      },
+      {
+        size: 'medium',
+        inventory: 20
+      },
+      {
+        size: 'large',
+        inventory: 25
+      },
+      {
+        size: 'x-large',
+        inventory: 20
+      }
+    ]
+  },
+  {
+    id: 8,
+    name: "Product 9",
+    price: 29.32,
+    photo: "9.jpg",
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: 5
+      },
+      {
+        size: 'small',
+        inventory: 12
+      },
+      {
+        size: 'medium',
+        inventory: 20
+      },
+      {
+        size: 'large',
+        inventory: 25
+      },
+      {
+        size: 'x-large',
+        inventory: 20
+      }
+    ]
+  },
+  {
+    id: 9,
+    name: "Product 10",
+    price: 13.52,
+    photo: "10.jpg",
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: 5
+      },
+      {
+        size: 'small',
+        inventory: 12
+      },
+      {
+        size: 'medium',
+        inventory: 20
+      },
+      {
+        size: 'large',
+        inventory: 25
+      },
+      {
+        size: 'x-large',
+        inventory: 20
+      }
+    ]
+  },
+  {
+    id: 10,
+    name: "Empty Product Test",
+    price: null,
+    photo: null,
+    sizes: [
+      {
+        size: 'x-small',
+        inventory: null
+      },
+      {
+        size: 'small',
+        inventory: null
+      },
+      {
+        size: 'medium',
+        inventory: null
+      },
+      {
+        size: 'large',
+        inventory: null
+      },
+      {
+        size: 'x-large',
+        inventory: null
+      }
+    ]
+  },
+];
