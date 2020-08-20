a83a6d8730751320471f00d709ac938c9ae7b02b
Jake <jakebirkes@gmail.com>
2020-08-19 19:46:00 -0700

    init storeConfig

---
+++ src/data/storeConfig.js
@@ -0,0 +1,13 @@
+export const storeConfig = {
+  siteURL: "",
+  siteTitle: "React Storefront",
+  siteDescription: "A shopping experience demo made with React Native",
+  siteAuthor: {
+    name: "Jake Wantulok",
+    email: "wantulok.jake@gmail.com",
+    github: "https://github.com/jakebirkes/react-storefront",
+    photo: "",
+    description: ""
+  },
+  copyright: `Copyright © ${new Date().getFullYear()}. React Storefront`
+};
