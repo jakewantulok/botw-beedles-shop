80cf33cd2728e64f852fcf431f087c50c42f8085
Jake <jakebirkes@gmail.com>
2020-08-19 20:52:01 -0700

    init CartProducts

---
+++ src/components/CartProducts/CartProducts.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
+import CartItem from '../../components/CartItem/CartItem';
+
+const CartProducts = () => {
+    const { cartItems } = useContext(CartContext);
+    const CartProduct = () => cartItems.map(product => <CartItem key={product.id} product={product}/>);
+
+    return ( 
+        <div className="card card-body border-0">
+            {CartProduct()}
+        </div>
+     );
+}
+ 
+export default CartProducts;
