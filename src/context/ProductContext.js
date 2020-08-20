462c9fa40d23f7a5e286203cc706956a882c9179
Jake <jakebirkes@gmail.com>
2020-08-19 20:54:55 -0700

    init ProductContext

---
+++ src/context/ProductContext.js
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from 'react';
+import { productData } from '../data/productData';
+export const ProductContext = createContext()
+
+const ProductsContextProvider = ({ children }) => {
+
+    const [ products ] = useState(productData);
+
+    return ( 
+        <ProductContext.Provider value={ { products } } >
+            { children }
+        </ProductContext.Provider>
+     );
+};
+ 
+export default ProductsContextProvider;
