cdbd38f6fe31ce8c6e003b707221dcc7380cb44d
Jake Wantulok <wantulok.jake@gmail.com>
2020-09-07 10:08:47 -0700

    single quotes

--- src/components/CartProducts.js
+++ src/components/CartProducts.js
@@ -7,25 +7,27 @@ const CartProducts = () => {
 	const { cartItems, total, savings, itemCount, handleCheckout, clearCart } = useContext(CartContext);
 	return (
 		<>
-			<div className="card card-body border-0">
+			<div className='card card-body border-0'>
 				{cartItems.map(product => <CartItem key={product.id} product={product} />)}
 			</div>
-			<div id="goToCheckout" className="card card-body col-sm-3 p-3">
-			<p className="mb-1">Total Items</p>
-			<h4 className="mb-3 txt-right">
+			<div id='goToCheckout' className='card card-body col-sm-3 p-3'>
+			<p className='mb-1'>Total Items</p>
+			<h4 className='mb-3 txt-right'>
 				{itemCount}
 			</h4>
-			<p className="mb-1">Total Payment</p>
-			<h3 className="m-0 txt-right">
+			<p className='mb-1'>Total Payment</p>
+			<h3 className='m-0 txt-right'>
 				{FormatCurrency(total)}
 			</h3>
-			<hr className="my-4" />
-			<p hidden={savings <= 0} className='text-success'>You saved {FormatCurrency(savings)}!</p>
-			<div className="text-center">
-				<button type="button" className="btn btn-primary mb-2" onClick={handleCheckout}>
+			<hr className='my-4' />
+			<p hidden={savings <= 0} className='text-success'>
+				You saved {FormatCurrency(savings)}!
+			</p>
+			<div className='text-center'>
+				<button type='button' className='btn btn-primary mb-2' onClick={handleCheckout}>
 					CHECKOUT
 				</button>
-				<button type="button" className="btn btn-outlineprimary btn-sm" onClick={clearCart}>
+				<button type='button' className='btn btn-outlineprimary btn-sm' onClick={clearCart}>
 					CLEAR ALL
 				</button>
 			</div>
