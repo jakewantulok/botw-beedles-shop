e73fb5f8a6c3ec4551e6daac5a70b5ae06b78d8d
Jake <jakebirkes@gmail.com>
2020-09-07 15:53:52 -0700

    if not in cart show within quantity range

--- src/components/QtyMsg.js
+++ src/components/QtyMsg.js
@@ -3,19 +3,22 @@ import { CartContext } from '../context/CartContext';
 import { InCart } from './InCart';
 
 const QtyMsg = props => {
-  const { cartItems } = useContext(CartContext);
-  const { item } = props;
-  return (
-    <span 
-      hidden={item.quantity > 0 
-        && InCart(item, cartItems) 
-        && (item.quantity - InCart(item, cartItems, 'cart') <= 15 && item.quantity - InCart(item, cartItems, 'cart') > 0) 
-        ? false : true} 
-      className='sizeQty text-danger'>
+	const { cartItems } = useContext(CartContext);
+	const { item } = props;
+	return (
+		<span
+			hidden={
+				(InCart(item, cartItems) &&
+					item.quantity - InCart(item, cartItems, 'cart') <= 15 &&
+					item.quantity - InCart(item, cartItems, 'cart') > 0) ||
+				(item.quantity > 0 && item.quantity <= 15)
+					? false
+					: true
+			}
+			className="sizeQty text-danger">
 			ONLY {InCart(item, cartItems) ? item.quantity - InCart(item, cartItems, 'cart') : item.quantity} LEFT!
-    </span>
-  );
-
+		</span>
+	);
 };
 
 export default QtyMsg;
