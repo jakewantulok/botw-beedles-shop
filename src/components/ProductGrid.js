c124a729b7a793ab7549c33e245c85bc14d25887
Jake <jakebirkes@gmail.com>
2020-09-12 22:57:53 -0700

    prettier autoformat

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -5,9 +5,7 @@ import { ProductContext } from '../context/ProductContext';
 const ProductGrid = () => {
 	const { products } = useContext(ProductContext);
 
-	const renderProducts = products.map(product => (
-		<ProductItem key={product.id} product={product} />
-	));
+	const renderProducts = products.map(product => <ProductItem key={product.id} product={product} />);
 
 	return (
 		<div>
