59a98230cb2a6bfcdf954669a5a176b5181082be
Jake <jakebirkes@gmail.com>
2020-09-12 22:57:36 -0700

    rupees as currency

---
+++ src/data/currencyData.js
@@ -0,0 +1,32 @@
+export const currencyData = [
+	{
+		id: 0,
+		name: 'green',
+		value: 1,
+	},
+	{
+		id: 1,
+		name: 'blue',
+		value: 5,
+	},
+	{
+		id: 2,
+		name: 'red',
+		value: 20,
+	},
+	{
+		id: 3,
+		name: 'purple',
+		value: 50,
+	},
+	{
+		id: 4,
+		name: 'silver',
+		value: 100,
+	},
+	{
+		id: 5,
+		name: 'gold',
+		value: 300,
+	},
+];
