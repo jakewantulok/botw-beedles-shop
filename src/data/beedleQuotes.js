58a8d081c0fe7839ffb339053f4fb9a7672d467e
Jake <jakebirkes@gmail.com>
2020-09-13 02:31:42 -0700

    quotes from beedle

---
+++ src/data/beedleQuotes.js
@@ -0,0 +1,54 @@
+export const beedleQuotes = [
+	{
+		id: 0,
+		condition: 'greeting',
+		quote: 'How can I help you today?',
+	},
+	{
+		id: 0,
+		condition: 'greeting',
+		quote: "I'm sure I have something you'll find useful! Please have a good look!",
+	},
+	{
+		id: 0,
+		condition: 'return',
+		quote: "Oh! I knew I'd see you again.\nI think that technically makes you my stalker! HAHAHAHA!",
+	},
+	{
+		id: 0,
+		condition: 'return',
+		quote: 'Whaaa? You came to see me again?!\nThat makes Beedle SO HAPPY!',
+	},
+	{
+		id: 0,
+		condition: 'checkout',
+		quote: 'Thanks for the business!\nThank you very much!\nHope to see you again soon!',
+	},
+	{
+		id: 0,
+		condition: 'return',
+		quote: "Hey! You're a great customer, and since today is Customer Appreciation Day...\nI want you to have this!",
+		gift: 'Ancient Arrow x1',
+	},
+	{
+		id: 0,
+		condition: 'return',
+		quote: "Oh! It's you again. Was there something you forgot to buy?",
+		gift: 'Ancient Arrow x1',
+	},
+	{
+		id: 0,
+		condition: 'return',
+		quote: 'Yah! We meet again!',
+	},
+	{
+		id: 0,
+		condition: 'return',
+		quote: "Hey! It's you again.\nOl' Beedle's glad to see you!",
+	},
+	{
+		id: 0,
+		condition: 'clear',
+		quote: "Oh! That's too bad...\nHope to see you around.",
+	},
+];
