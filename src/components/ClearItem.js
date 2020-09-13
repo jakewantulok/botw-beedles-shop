41ac34a7a0d81c879aa4822ba90e463f0d3a16b2
Jake <jakebirkes@gmail.com>
2020-09-13 02:31:00 -0700

    clear based on name

--- src/components/ClearItem.js
+++ src/components/ClearItem.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 
 const ClearItem = props => {
-  const { item, children } = props;
-  const { removeProduct, cartItems } = useContext(CartContext);
-  const hideOnClear = () => (!cartItems.find(obj => obj.id === item.id));
-  return (
-    <button hidden={hideOnClear()} onClick={() => removeProduct(item)} className='btn btn-sm text-dark'>{ children || 'remove' }</button>
-  );
+	const { item, children } = props;
+	const { removeProduct, cartItems } = useContext(CartContext);
+	const hideOnClear = () => !cartItems.find(obj => obj.name === item.name);
+	return (
+		<button hidden={hideOnClear()} onClick={() => removeProduct(item)} className="btn btn-sm text-dark">
+			{children || 'remove'}
+		</button>
+	);
 };
 
 export default ClearItem;
