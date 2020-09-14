447cb9936fe7e2783d66d6a17dadfd28f4eeff7e
Jake <jakebirkes@gmail.com>
2020-09-13 18:40:31 -0700

    'CLEAR' as default text

--- src/components/ClearItem.js
+++ src/components/ClearItem.js
@@ -6,8 +6,8 @@ const ClearItem = props => {
 	const { removeProduct, cartItems } = useContext(CartContext);
 	const hideOnClear = () => !cartItems.find(obj => obj.name === item.name);
 	return (
-		<button hidden={hideOnClear()} onClick={() => removeProduct(item)} className="btn btn-sm text-dark">
-			{children || 'remove'}
+		<button hidden={hideOnClear()} onClick={() => removeProduct(item)} className="btn btn-sm text-danger">
+			{children || 'CLEAR'}
 		</button>
 	);
 };
