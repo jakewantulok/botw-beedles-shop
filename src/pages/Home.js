726094c51e90a327456cb6c377108aa94904be71
Jake <jakebirkes@gmail.com>
2020-09-19 10:57:20 -0700

    name change, all products by default

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import ProductsGrid from '../components/ProductGrid';
 export const Home = () => (
 	<>
 		<div className="text-center mt-5">
-			<h1>Store</h1>
+			<h1>All</h1>
 		</div>
 		<ProductsGrid />
 	</>
