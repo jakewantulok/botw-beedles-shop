7c4e67c606f8f824b7bd56e74cb4fbb0bd46f6d7
Jake <jakebirkes@gmail.com>
2020-09-19 10:58:42 -0700

    add all of inventory

--- src/data/productData.js
+++ src/data/productData.js
@@ -182,14 +182,284 @@ export const productData = [
 			},
 		],
 	},
-
 	{
 		id: 6,
 		name: 'Octo Balloon',
-		category: 'Monster Part',
+		category: 'Monster Parts',
 		photo: 'octo_balloon.png',
 		description:
 			'Daggers created using ancient technology have been repurposed for use as arrows. Extremely effective against Guardians.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 7,
+		name: 'Cold Darner',
+		category: 'Darners',
+		photo: 'cold_darner.png',
+		description:
+			'Daggers created using ancient technology have been repurposed for use as arrows. Extremely effective against Guardians.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 8,
+		name: 'Warm Darner',
+		category: 'Darners',
+		photo: 'warm_darner.png',
+		description:
+			'Daggers created using ancient technology have been repurposed for use as arrows. Extremely effective against Guardians.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 9,
+		name: 'Electric Darner',
+		category: 'Darners',
+		photo: 'electric_darner.png',
+		description:
+			'Daggers created using ancient technology have been repurposed for use as arrows. Extremely effective against Guardians.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 10,
+		name: 'Hearty Lizard',
+		category: 'Lizards',
+		photo: 'hearty_lizard.png',
+		description:
+			'This rare lizard lives deep in the forests. It feeds on high-nutrient foods, giving it great vitality. When used to make elixirs, they temporarily increase your maximum hearts.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 11,
+		name: 'Fireproof Lizard',
+		category: 'Lizards',
+		photo: 'fireproof_lizard.png',
+		description:
+			'This rare lizard can only be found in the Eldin region. Its scales have heat-resistant properties, so when cooked with monster parts, it produces a heat-resistance elixir.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 12,
+		name: 'Hightail Lizard',
+		category: 'Lizards',
+		photo: 'hightail_lizard.png',
+		description:
+			"A lizard found throughout Hyrule. It's a bit slow to react at times, but if given a chance to escape, it will dart off quickly. Cook it with monster parts for a speed-boosting elixir.",
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 13,
+		name: 'Hot-footed Frog',
+		category: 'Frogs',
+		photo: 'hot-footed_frog.png',
+		description:
+			'A quick frog that can be found hopping around near water. Cook it with monster parts to draw out its speed-boost effect.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 14,
+		name: 'Tireless Frog',
+		category: 'Frogs',
+		photo: 'tireless_frog.png',
+		description:
+			'This rare frog only ventures out in the rain. When cooked with monster parts, the elixir it produces will temporarily increase your maximum stamina.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 15,
+		name: 'Restless Cricket',
+		category: 'Crickets',
+		photo: 'restless_cricket.png',
+		description: 'A very energetic cricket. Cook it with monster parts to create a stamina-recovery elixir.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 16,
+		name: 'Smotherwing Butterfly',
+		category: 'Butterflies',
+		photo: 'smotherwing_butterfly.png',
+		description:
+			'This rare butterfly lives in volcanic regions. Its body contains a heat-resistant liquid, which can be turned into a topical elixir that offers resistance to flames.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 17,
+		name: 'Summerwing Butterfly',
+		category: 'Butterflies',
+		photo: 'summerwing_butterfly.png',
+		description:
+			'Daggers created using ancient technology have been repurposed for use as arrows. Extremely effective against Guardians.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 18,
+		name: 'Thunderwing Butterfly',
+		category: 'Butterflies',
+		photo: 'thunderwing_butterfly.png',
+		description:
+			'This rare butterfly only shows itself when it rains. The organs in its body produce an insulating compound. When made into an elixir, it offers electrical resistance.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 19,
+		name: 'Winterwing Butterfly',
+		category: 'Butterflies',
+		photo: 'winterwing_butterfly.png',
+		description:
+			"The powdery scales of this butterfly's wings cool the air around it. Watching it flutter around snowflakes is a thing of beauty. Cook it with monster parts for a heat-resistant elixir.",
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 20,
+		name: 'Sunset Firefly',
+		category: 'Firelies',
+		photo: 'sunset_firefly.png',
+		description:
+			'These fireflies glow gently in the dark. When cooked with monster parts, the compound that causes it to glow results in an elixir that will allow you to move more quietly.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 21,
+		name: 'Hearty Radish',
+		category: 'Ingredients',
+		photo: 'hearty_radish.png',
+		description:
+			'A rare radish that grows best in sunny plains. Cook it before eating to temporarily increase your maximum health.',
+		quantity: 25,
+		price: 20,
+		sale: [
+			{
+				name: '',
+				bulk: 1,
+				price: 20,
+			},
+		],
+	},
+	{
+		id: 22,
+		name: 'Swift Carrot',
+		category: 'Ingredients',
+		photo: 'swift_carrot.png',
+		description:
+			'This carrot is cultivated extensively in Kakariko Village. It strengthens the legs and hips when cooked into a dish, which helps increase your movement speed.',
 		quantity: 25,
 		price: 20,
 		sale: [
