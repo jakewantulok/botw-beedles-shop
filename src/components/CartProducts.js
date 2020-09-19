36e9f6789d39afadd27e86b17b81807a7a5ab3ec
Jake <jakebirkes@gmail.com>
2020-09-19 10:57:58 -0700

    cart info

--- src/components/CartProducts.js
+++ src/components/CartProducts.js
@@ -1,12 +1,49 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import { CartContext } from '../context/CartContext';
 import CartItem from './CartItem';
+
+const Info = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	& div {
+		padding-left: 0;
+		& div {
+			padding: 15px;
+			border-radius: 15px;
+			transition: background-color 0.5s;
+			transition: border 0.5s;
+			background-color: rgba(22, 22, 29, 90%);
+			border: 1px solid #16161d00;
+		}
+		&:nth-child(3) div {
+			margin-right: -15px;
+		}
+	}
+`;
 
 const CartProducts = () => {
 	const { cartItems, total, savings, savingsPercent, itemCount, handleCheckout, clearCart } = useContext(CartContext);
 	return (
 		<>
 			<div>
+				<Info>
+					<div class="col-sm-6">
+						<div>
+							<h4>Items</h4>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div>
+							<h4>Stock</h4>
+						</div>
+					</div>
+					<div class="col-sm-3">
+						<div>
+							<h4>Price</h4>
+						</div>
+					</div>
+				</Info>
 				{cartItems.map(product => (
 					<CartItem key={product.id} product={product} />
 				))}
