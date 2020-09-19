1e2409985a126998d3ea1678c0f476d59837c437
Jake <jakebirkes@gmail.com>
2020-09-19 10:58:14 -0700

    background for products

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -1,12 +1,20 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import ProductItem from './ProductItem';
 import { ProductContext } from '../context/ProductContext';
 
+const Products = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	background: rgba(22, 22, 29, 90%);
+	border-radius: 15px;
+	padding: 15px;
+	margin: 15px 0;
+`;
+
 const ProductGrid = () => {
 	const { products } = useContext(ProductContext);
-
 	const renderProducts = products.map(product => <ProductItem key={product.id} product={product} />);
-
 	return (
 		<>
 			<div className="row">
@@ -14,7 +22,7 @@ const ProductGrid = () => {
 					<div className="py-3">{products.length} Products</div>
 				</div>
 			</div>
-			<div className="row">{renderProducts}</div>
+			<Products>{renderProducts}</Products>
 		</>
 	);
 };
