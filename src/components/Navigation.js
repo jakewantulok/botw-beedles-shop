e71dfa87d6d6030c01e31019fd4e2fac15338ff4
Jake <jakebirkes@gmail.com>
2020-09-21 19:52:54 -0700

    wrap nav in container

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
-import { Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { CartIcon } from './Icons';
 import { storeConfig } from '../data/storeConfig';
 import BeedleBanner from './BeedleBanner';
@@ -13,34 +13,36 @@ const Navigation = props => {
 
 	return (
 		<Navbar bg={theme} className={theme === 'light' ? 'navbar-light' : 'navbar-dark'} expand="md">
-			<a href="/">
-				<BeedleBanner />
-			</a>
-			<Navbar.Brand href="/" style={{ fontFamily: 'Sherwood', fontSize: 40, fontWeight: 'normal' }}>
-				{siteTitle}
-			</Navbar.Brand>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="mr-auto">
-					<Nav.Link href="/">Home</Nav.Link>
-					<Nav.Link href="/cart">
-						<CartIcon width={20} />
-						Cart ({itemCount})
-					</Nav.Link>
-					<button onClick={() => themeHandler()} />
-					{/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+			<Container>
+				<a href="/">
+					<BeedleBanner />
+				</a>
+				<Navbar.Brand href="/" style={{ fontFamily: 'Sherwood', fontSize: 40, fontWeight: 'normal' }}>
+					{siteTitle}
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link href="/">Home</Nav.Link>
+						<Nav.Link href="/cart">
+							<CartIcon width={20} />
+							Cart ({itemCount})
+						</Nav.Link>
+						<button onClick={() => themeHandler()} />
+						{/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="action/3.2">Another action</NavDropdown.Item>
             <NavDropdown.Item href="action/3.3">Something</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="action/3.4">Separated link</NavDropdown.Item>
           </NavDropdown> */}
-				</Nav>
-				{/* <Form inline>
+					</Nav>
+					{/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
         </Form> */}
-			</Navbar.Collapse>
+				</Navbar.Collapse>
+			</Container>
 		</Navbar>
 	);
 };
