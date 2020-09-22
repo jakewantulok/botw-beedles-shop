05b25d7d49a6cfd165a0331f29e6446b9e98473d
Jake <jakebirkes@gmail.com>
2020-09-21 19:53:14 -0700

    add dropdown back

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -12,7 +12,6 @@ const Products = styled.div`
 	background: rgba(22, 22, 29, 50%);
 	border-radius: 15px;
 	padding: 15px;
-	margin: 15px 0;
 	box-shadow: 5px 5px 10px #16161d50;
 `;
 
@@ -43,11 +42,11 @@ const ProductGrid = () => {
 
 	let filtersArr = [...products.map(product => product.category)];
 
-	// const filters = [...new Set(filtersArr)].map((option, i) => (
-	// 	<Dropdown.Item key={i} onClick={() => handleFilter(option)}>
-	// 		{option}
-	// 	</Dropdown.Item>
-	// ));
+	const dropdownFilters = [...new Set(filtersArr)].map((option, i) => (
+		<Dropdown.Item key={i} onClick={() => handleFilter(option)}>
+			{option}
+		</Dropdown.Item>
+	));
 
 	const filterBtns = [...new Set(filtersArr)].map((option, i) => (
 		<button
@@ -68,38 +67,34 @@ const ProductGrid = () => {
 	return (
 		<>
 			<div className="row">
-				<div className="col-12 col-sm-2">
-					<div className="py-3">
-						<Filter>
-							<span>Filter: </span>
-							{/* <Dropdown>
-								<Dropdown.Toggle variant="success" id="dropdown-basic"></Dropdown.Toggle>
-								<Dropdown.Menu>
-									<Dropdown.Item onClick={() => handleFilter('All')}>All</Dropdown.Item>
-									{filters}
-								</Dropdown.Menu>
-							</Dropdown> */}
-
+				<div className="col-0 col-sm-0 col-md-2">
+					<Filter>
+						<Dropdown hidden>
+							<Dropdown.Toggle variant="primary" id="dropdown-basic">
+								{filter}
+							</Dropdown.Toggle>
+							<Dropdown.Menu>
+								<Dropdown.Item onClick={() => handleFilter('All')}>All</Dropdown.Item>
+								{dropdownFilters}
+							</Dropdown.Menu>
+						</Dropdown>
+						<div className="text-center">
 							<span>
 								{renderProducts.length > 1 ? renderProducts.length + ' Products' : renderProducts.length + ' Product'}
 							</span>
-							<FilterOptions>
-								{/* <Button variant="dark" hidden={filter === 'All'} onClick={() => handleFilter('All')}>
-									<CloseIcon width={22} color={'white'} />
-									{filter}
-								</Button> */}
-								<button
-									className={filter === 'All' ? 'btn btn-primary' : 'btn btn-outline-light'}
-									onClick={() => handleFilter('All')}>
-									All
-								</button>
-								{filterBtns}
-							</FilterOptions>
-						</Filter>
-					</div>
+						</div>
+						<FilterOptions>
+							<button
+								className={filter === 'All' ? 'btn btn-primary' : 'btn btn-outline-light'}
+								onClick={() => handleFilter('All')}>
+								All
+							</button>
+							{filterBtns}
+						</FilterOptions>
+					</Filter>
 				</div>
-				<div className="col-12 col-sm-1" />
-				<div className="col-12 col-sm-9">
+				{/* <div className="col-0 col-sm-0 col-md-1" /> */}
+				<div className="col-12 col-sm-12 col-md-10">
 					<Products>{renderProducts}</Products>
 				</div>
 			</div>
