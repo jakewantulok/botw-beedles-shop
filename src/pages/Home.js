5b6cfc33a531826d4b963587e2a2de93db3c13a2
Jake <jakebirkes@gmail.com>
2020-10-24 12:00:22 -0700

    header moved to ProductGrid

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,4 @@
 import React from 'react';
 import ProductsGrid from '../components/ProductGrid';
 
-export const Home = () => (
-	<>
-		<div className="text-center mt-5">
-			<h1>All</h1>
-		</div>
-		<ProductsGrid />
-	</>
-);
+export const Home = () => <ProductsGrid />;
