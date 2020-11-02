4dd1af4d47af0707ebe157dfe7a169b79c79a5ea
Jake <jakebirkes@gmail.com>
2020-11-01 17:24:54 -0800

    update navigation styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,41 @@
 import React, { useContext } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { CartIcon } from './Icons';
+import { CartIcon, MoonIcon, SunIcon } from './Icons';
 import { storeConfig } from '../data/storeConfig';
 import BeedleBanner from './BeedleBanner';
 import { CartContext } from '../context/CartContext';
-// #245e79
+import styled from 'styled-components';
+
+const ThemeBtn = styled.button`
+	padding: 0;
+	border: none;
+	border-radius: 50%;
+	width: 40px;
+	height: 40px;
+	box-shadow: 5px 5px 10px #16161d50;
+`;
+
+const NavBrandWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	& .shop-title {
+		font-family: Sherwood;
+		font-size: 30px;
+		font-weight: normal;
+	}
+`;
+
+const MobileThemeWrapper = styled.div`
+	position: relative;
+	width: 90%;
+	height: 0;
+	& button {
+		position: absolute;
+		top: 115px;
+		right: -5px;
+		z-index: 9999;
+	}
+`;
 
 const Navigation = props => {
 	const { siteTitle } = storeConfig;
@@ -12,27 +43,48 @@ const Navigation = props => {
 	const { themeHandler, theme } = props;
 
 	return (
-		<Navbar bg={theme} className={theme === 'light' ? 'navbar-light' : 'navbar-dark'} expand="md">
-			<Container>
-				<a href="/">
-					<BeedleBanner />
-				</a>
-				<Navbar.Brand href="/" style={{ fontFamily: 'Sherwood', fontSize: 40, fontWeight: 'normal' }}>
-					{siteTitle}
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="mr-auto">
-						<Nav.Link href="/">Home</Nav.Link>
-						<Nav.Link href="/cart">
-							<CartIcon width={20} />
-							Cart ({cartItems.length || 0})
-						</Nav.Link>
-						<button onClick={() => themeHandler()} />
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
+		<>
+			<MobileThemeWrapper>
+				<ThemeBtn
+					className={theme === 'dark' ? 'btn-warning btn-theme mobile' : 'btn-filter btn-theme mobile'}
+					onClick={() => themeHandler()}>
+					{theme === 'dark' ? <SunIcon color="#16161d" /> : <MoonIcon color="#eeeeee" />}
+				</ThemeBtn>
+			</MobileThemeWrapper>
+			<Navbar
+				id="main-navigation"
+				className={theme === 'light' ? 'navbar-light light' : 'navbar-dark dark'}
+				expand="md">
+				<Container>
+					<NavBrandWrapper>
+						<a href="/">
+							<BeedleBanner />
+						</a>
+						<Navbar.Brand href="/">
+							<span className="shop-title expanded">{siteTitle}</span>
+							<span className="shop-title mobile">{siteTitle.split(' ')[0]}</span>
+						</Navbar.Brand>
+					</NavBrandWrapper>
+
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+						<Nav>
+							<Nav.Link href="/">Home</Nav.Link>
+							<Nav.Link href="/cart">
+								<CartIcon width={20} />
+								Cart ( {cartItems.length || 0} )
+							</Nav.Link>
+							<ThemeBtn
+								className={theme === 'dark' ? 'btn-warning btn-theme' : 'btn-filter btn-theme'}
+								style={{ marginLeft: '25px' }}
+								onClick={() => themeHandler()}>
+								{theme === 'dark' ? <SunIcon color="#16161d" /> : <MoonIcon color="#eeeeee" />}
+							</ThemeBtn>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
 	);
 };
 
--- src/components/global.js
+++ src/components/global.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import { MenuIcon } from './Icons';
 
 export const GlobalStyles = createGlobalStyle`
   *,
@@ -19,6 +21,63 @@ export const GlobalStyles = createGlobalStyle`
   .btn-filter {
     color: white;
     background-color: #6f42c1;
+  }
+
+  hr {
+    border-top: 1px solid white;
+  }
+
+  #textInputCoupon {
+    margin-bottom: 20px;
+    border-radius: 10px;
+    padding: 10px;
+    border: none;
+  }
+
+  .navbar-light, 
+  .navbar-dark {
+    background-color: rgba(22,22,29,50%);
+  }
+
+  .navbar-light .navbar-brand, 
+  .navbar-dark .navbar-brand,
+  .navbar-light .navbar-nav .nav-link,
+  .navbar-dark .navbar-nav .nav-link {
+    color: white;
+    &:hover {
+      color: #17a2b8;
+    }
+  }
+
+  .navbar-light .navbar-toggler-icon,
+  .navbar-dark .navbar-toggler-icon {
+    background-image: url(./menu.svg);
+    width: 40px;
+    height: 40px;
+  }
+
+  .navbar-light .navbar-toggler,
+  .navbar-dark .navbar-toggler {
+    border: none;
+    padding: 0;
+  }
+
+  .navbar-collapse {
+    margin-left: 75px;
+  }
+
+  @media (max-width: 767px) {
+    .navbar-collapse .btn-theme,
+    .shop-title.expanded {
+      display: none;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .btn-theme.mobile,
+    .shop-title.mobile {
+      display: none;
+    }
   }
 
 `;
