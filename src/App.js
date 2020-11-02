3252d94daa950b015505b9a26a055457c820b7e4
Jake Wantulok <wantulok.jake@gmail.com>
2020-11-01 20:34:33 -0800

    set theme in local storage

--- src/App.js
+++ src/App.js
@@ -11,8 +11,14 @@ import Navigation from './components/Navigation';
 import { Router } from './Routes/Router';
 
 const App = () => {
-	const [mode, setMode] = useState('light');
-	const handleThemeChange = () => setMode(mode === 'dark' ? 'light' : 'dark');
+	!localStorage.getItem('theme') && localStorage.setItem('theme', JSON.stringify('light'));
+	const initMode = localStorage.getItem('theme') ? JSON.parse(localStorage.getItem('theme')) : 'light';
+
+	const [mode, setMode] = useState(initMode);
+	const handleThemeChange = () => {
+		setMode(mode === 'dark' ? 'light' : 'dark');
+		localStorage.setItem('theme', JSON.stringify(mode === 'dark' ? 'light' : 'dark'));
+	};
 	const toggleTheme = useMemo(() => mode === 'dark', [mode]);
 
 	return (
